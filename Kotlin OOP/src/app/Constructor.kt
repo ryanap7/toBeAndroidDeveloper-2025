a5f5155c17de60cb08f96ac8301ce034023ca6cb
ryanap7 <ryan.aprianto77777@gmail.com>
2025-09-04 11:34:42 +0700

    feat: Constructor in Kotlin OOP

---
+++ Kotlin OOP/src/app/Constructor.kt
@@ -0,0 +1,9 @@
+package app
+
+import data.Car
+
+fun main() {
+    val avanza = Car("Toyota", 2015)
+
+    println(avanza.brand)
+}
--- Kotlin OOP/src/data/Car.kt
+++ Kotlin OOP/src/data/Car.kt
@@ -1,6 +1,6 @@
 package data
 
-class Car {
-    var brand: String = ""
-    var year: Int = 2020
+class Car(brand: String, year: Int = 2020) {
+    var brand: String = brand
+    var year: Int = year
 }
