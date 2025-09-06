1cdadbed7f7ab106a2bcb1dd1e59a2dc4c3d69e2
ryanap7 <ryan.aprianto77777@gmail.com>
2025-09-06 14:21:21 +0700

    feat: Any Class in Kotlin OOP

---
+++ Kotlin OOP/src/app/AnyClass.kt
@@ -0,0 +1,9 @@
+package app
+
+import data.SmartPhone
+
+fun main() {
+    val smartPhone = SmartPhone("Samsung S20", "Android")
+
+    println(smartPhone.toString())
+}
---
+++ Kotlin OOP/src/data/Laptop.kt
@@ -0,0 +1,6 @@
+package data
+
+class Laptop(val name: String)
+open class HandPhone(val name: String)
+
+class SmartPhone (name: String, val os: String) : HandPhone(name)
